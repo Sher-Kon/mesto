994de70825f6f497a95552f4ac6c7cda2d02bf2e
Sher-Kon <sher-kon@ya.ru>
2021-10-14 14:26:44 +0300

    кнопки заработали

--- script.js
+++ script.js
@@ -2,16 +2,19 @@ let content = document.querySelector('.content');
 let editButton = content.querySelector('.profile__info-edit-btn');
 let addButton = content.querySelector('.profile__add-btn');
 
-let memButton = popup.querySelector('.form__submit-btn-mem');
+let memButton = content.querySelector('.popup__btn-mem');
+
+let popupElement = content.querySelector('.popup');
 
 function showClickEdit() {
-    //href="#popup";
+    popupElement.classList.add('popup_opened');
     console.log('Мы кликнули по элементу EDIT');
   }
   function showClickAdd() {
     console.log('Мы кликнули по элементу ADD');
   }
   function showClickMem() {
+    popupElement.classList.remove('popup_opened');
     console.log('Мы кликнули по элементу MEM');
   }
 
