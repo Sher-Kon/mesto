a4b691defe993ad14b9103b36d195babeaa158f0
Sher-Kon <sher-kon@ya.ru>
2021-10-15 14:07:33 +0300

    отформатировал все

--- script.js
+++ script.js
@@ -1,52 +1,50 @@
-let content = document.querySelector('.content');
-let editButton = content.querySelector('.profile__info-edit-btn');
-let addButton = content.querySelector('.profile__add-btn');
-let saveButton = content.querySelector('.popup__btn-save');
-let closeButton = content.querySelector('.popup__btn-close');
-let popupElement = content.querySelector('.popup');
+let content = document.querySelector(".content");
+let editButton = content.querySelector(".profile__info-edit-btn");
+let addButton = content.querySelector(".profile__add-btn");
+let saveButton = content.querySelector(".popup__btn-save");
+let closeButton = content.querySelector(".popup__btn-close");
+let popupElement = content.querySelector(".popup");
 
 function ClickEdit() {
-  let name = document.querySelector('.profile__info-title');
-  let subname = document.querySelector('.profile__info-subtitle');
-  console.log(name.textContent);
-  console.log(subname.textContent);
+    let name = document.querySelector(".profile__info-title");
+    let subname = document.querySelector(".profile__info-subtitle");
+    console.log(name.textContent);
+    console.log(subname.textContent);
 
-  let memname = document.querySelector('.popup__text-name');
-  let membrif = document.querySelector('.popup__text-brif');
-  memname.value = name.textContent;
-  membrif.value = subname.textContent;
+    let memname = document.querySelector(".popup__text-name");
+    let membrif = document.querySelector(".popup__text-brif");
+    memname.value = name.textContent;
+    membrif.value = subname.textContent;
 
-    popupElement.classList.add('popup_opened');//открыть окно
-    console.log('Мы кликнули по элементу EDIT');
+    popupElement.classList.add("popup_opened"); //открыть окно
+    console.log("Мы кликнули по элементу EDIT");
 }
 
 function ClickAdd() {
-    console.log('Мы кликнули по элементу ADD');
+    console.log("Мы кликнули по элементу ADD");
 }
 
 function ClickSave() {
-    let memname = document.querySelector('.popup__text-name');
-    let membrif = document.querySelector('.popup__text-brif');
+    let memname = document.querySelector(".popup__text-name");
+    let membrif = document.querySelector(".popup__text-brif");
     console.log(memname.value);
     console.log(membrif.value);
 
-    let name = document.querySelector('.profile__info-title');
-    let subname = document.querySelector('.profile__info-subtitle');
+    let name = document.querySelector(".profile__info-title");
+    let subname = document.querySelector(".profile__info-subtitle");
     name.textContent = memname.value;
     subname.textContent = membrif.value;
-        
-    popupElement.classList.remove('popup_opened');//закрыть окно
-    console.log('Мы кликнули по элементу SAVE');
+
+    popupElement.classList.remove("popup_opened"); //закрыть окно
+    console.log("Мы кликнули по элементу SAVE");
 }
 
 function ClickClose() {
-  popupElement.classList.remove('popup_opened');//закрыть окно
-  console.log('Мы кликнули по элементу CLOSE');
+    popupElement.classList.remove("popup_opened"); //закрыть окно
+    console.log("Мы кликнули по элементу CLOSE");
 }
 
-
-  editButton.addEventListener('click', ClickEdit);
-  addButton.addEventListener('click', ClickAdd);
-  saveButton.addEventListener('click', ClickSave);
-  closeButton.addEventListener('click', ClickClose);
-  
+editButton.addEventListener("click", ClickEdit);
+addButton.addEventListener("click", ClickAdd);
+saveButton.addEventListener("click", ClickSave);
+closeButton.addEventListener("click", ClickClose);
