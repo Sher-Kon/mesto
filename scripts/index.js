7b287ca2059b02f05ee7a1425c39ee41ecbccc62
Sher-Kon <sher-kon@ya.ru>
2021-10-19 11:32:07 +0300

    форме и инпутам добавил атрибут name

--- scripts/index.js
+++ scripts/index.js
@@ -4,80 +4,37 @@ let editButton = content.querySelector(".profile__info-edit-btn");
 let addButton = content.querySelector(".profile__add-btn");
 let saveButton = content.querySelector(".popup__btn-save");
 let closeButton = content.querySelector(".popup__btn-close");
+let nameInfo = content.querySelector(".profile__info-name");
+let jobInfo = content.querySelector(".profile__info-job");
 let popupElement = content.querySelector(".popup");
+let nameInput = popupElement.querySelector(".popup__text-name");
+let jobInput = popupElement.querySelector(".popup__text-job");
 
+let formElement = document.querySelector(".form");// Находим форму в DOM
+
+// Обработчик открытия формы
 function ClickEdit() {
-    let name = document.querySelector(".profile__info-name");
-    let job = document.querySelector(".profile__info-job");
-    //console.log(name.textContent);
-    //console.log(subname.textContent);
-
-    let nameInput = document.querySelector(".popup__text-name");
-    let jobInput = document.querySelector(".popup__text-job");
-    nameInput.value = name.textContent;
-    jobInput.value = job.textContent;
-
+    nameInput.value = nameInfo.textContent;
+    jobInput.value = jobInfo.textContent;
     popupElement.classList.add("popup_opened"); //открыть окно
-    //console.log("Мы кликнули по элементу EDIT");
-}
-
-function ClickAdd() {
-    console.log("Мы кликнули по элементу ADD");
-}
-
-function ClickLike() {
-    console.log("Мы кликнули по элементу LIKE");
-}
-
-function ClickSave() {
-    let nameInput = document.querySelector(".popup__text-name");
-    let jobInput = document.querySelector(".popup__text-job");
-    //console.log(memname.value);
-    //console.log(membrif.value);
-
-    let name = document.querySelector(".profile__info-name");
-    let job = document.querySelector(".profile__info-job");
-    name.textContent = nameInput.value;
-    job.textContent = jobInput.value;
-
-    popupElement.classList.remove("popup_opened"); //закрыть окно
-    //console.log("Мы кликнули по элементу SAVE");
 }
 
 // Обработчик закрытия формы
 function ClickClose() {
     popupElement.classList.remove("popup_opened"); //закрыть окно
-    //console.log("Мы кликнули по кнопке CLOSE");
 }
-
-likeButton.addEventListener("click", ClickLike);
-editButton.addEventListener("click", ClickEdit);
-addButton.addEventListener("click", ClickAdd);
-closeButton.addEventListener("click", ClickClose);
-//saveButton.addEventListener("click", ClickSave);//для div - контейнера
-
-// Находим форму в DOM
-let formElement = document.querySelector(".form");
 
 // Обработчик «отправки» формы, хотя пока
 // она никуда отправляться не будет
-function formSubmitHandler (evt) {
+function formSubmitHandler(evt) {
     evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
-                                                // Так мы можем определить свою логику отправки.
-                                                // О том, как это делать, расскажем позже.
+    // Так мы можем определить свою логику отправки.
+    // О том, как это делать, расскажем позже.
     //console.log('Обработчик отправки формы');
 
-    // Получите значение полей jobInput и nameInput
-    let nameInput = document.querySelector(".popup__text-name");
-    let jobInput = document.querySelector(".popup__text-job");
-
-    // Выберите элементы, куда должны быть вставлены значения полей
-    let name = document.querySelector(".profile__info-name");
-    let job = document.querySelector(".profile__info-job");
-
     // Вставьте новые значения с помощью textContent
-    name.textContent = nameInput.value;
-    job.textContent = jobInput.value;
+    nameInfo.textContent = nameInput.value;
+    jobInfo.textContent = jobInput.value;
 
     // Закроем форму
     popupElement.classList.remove("popup_opened"); //закрыть окно
@@ -85,3 +42,6 @@ function formSubmitHandler (evt) {
 // Прикрепляем обработчик к форме:
 // он будет следить за событием “submit” - «отправка»
 formElement.addEventListener('submit', formSubmitHandler);
+
+editButton.addEventListener("click", ClickEdit);
+closeButton.addEventListener("click", ClickClose);
