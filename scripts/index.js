393a2b64bf4fc3fc495d5810bb6592d4d4bc72c0
Sher-Kon <sher-kon@ya.ru>
2021-10-27 12:57:59 +0300

    кавычки везде двойные

--- scripts/index.js
+++ scripts/index.js
@@ -82,114 +82,58 @@ function formSubmitHandler(evt) {
 }
 // Прикрепляем обработчик к форме:
 // он будет следить за событием “submit” - «отправка»
-formElement.addEventListener('submit', formSubmitHandler);
+formElement.addEventListener("submit", formSubmitHandler);
 
 editButton.addEventListener("click", clickEdit);//открыть попап
 closeButton.addEventListener("click", clickClose);//закрыть попап
 
-// выберем кнопку лайка
-const likeButtonA = content.querySelector(".element__like-btn_a");
-const likeButtonB = content.querySelector(".element__like-btn_b");
-const likeButtonC = content.querySelector(".element__like-btn_c");
-const likeButtonD = content.querySelector(".element__like-btn_d");
-const likeButtonE = content.querySelector(".element__like-btn_e");
-const likeButtonF = content.querySelector(".element__like-btn_f");
-// добавим обработчик
-likeButtonA.addEventListener("click", function() {
-  console.log("лайкнули A");
-  likeButtonA.classList.toggle("element__like-btn_active");
-});
-likeButtonB.addEventListener("click", function() {
-  console.log("лайкнули B");
-  likeButtonB.classList.toggle("element__like-btn_active");
-});
-likeButtonC.addEventListener("click", function() {
-  console.log("лайкнули C");
-  likeButtonC.classList.toggle("element__like-btn_active");
-});
-likeButtonD.addEventListener("click", function() {
-  console.log("лайкнули D");
-  likeButtonD.classList.toggle("element__like-btn_active");
-});
-likeButtonE.addEventListener("click", function() {
-  console.log("лайкнули E");
-  likeButtonE.classList.toggle("element__like-btn_active");
-});
-likeButtonF.addEventListener("click", function() {
-  console.log("лайкнули F");
-  likeButtonF.classList.toggle("element__like-btn_active");
-});
-
-// выберем кнопку удаления
-const deleteButtonA = document.querySelector('.element__del-btn_a');
-const deleteButtonB = document.querySelector('.element__del-btn_b');
-const deleteButtonC = document.querySelector('.element__del-btn_c');
-const deleteButtonD = document.querySelector('.element__del-btn_d');
-const deleteButtonE = document.querySelector('.element__del-btn_e');
-const deleteButtonF = document.querySelector('.element__del-btn_f');
-// добавим обработчик
-deleteButtonA.addEventListener('click', function () {
-  console.log("Кн. УДАЛИТЬ A");
-  const listItem = deleteButtonA.closest('.element');
-  listItem.remove();
-}); 
-deleteButtonB.addEventListener('click', function () {
-  console.log("Кн. УДАЛИТЬ B");
-  const listItem = deleteButtonB.closest('.element');
-  listItem.remove();
-}); 
-deleteButtonC.addEventListener('click', function () {
-  console.log("Кн. УДАЛИТЬ C");
-  const listItem = deleteButtonC.closest('.element');
-  listItem.remove();
-}); 
-deleteButtonD.addEventListener('click', function () {
-  console.log("Кн. УДАЛИТЬ D");
-  const listItem = deleteButtonD.closest('.element');
-  listItem.remove();
-}); 
-deleteButtonE.addEventListener('click', function () {
-  console.log("Кн. УДАЛИТЬ E");
-  const listItem = deleteButtonE.closest('.element');
-  listItem.remove();
-}); 
-deleteButtonF.addEventListener('click', function () {
-  console.log("Кн. УДАЛИТЬ F");
-  const listItem = deleteButtonF.closest('.element');
-  listItem.remove();
-}); 
-
-
-let index=0;
-// добавим обработчик кнопки +
-addButton.addEventListener("click", function() {
-
+// Клонируем карточку
+function addElement (url, txt) {
   // получим элемент 
-  const elementTemplate = document.querySelector('#element').content;//клон
-  const elementsOnline = document.querySelector('.elements');//куда вставить
+  const elementTemplate = document.querySelector("#element").content;//клон
+  const elementsOnline = document.querySelector(".elements");//куда вставить
   // клонируем содержимое тега template
-  const userElement = elementTemplate.querySelector('.element').cloneNode(true);
+  const userElement = elementTemplate.querySelector(".element").cloneNode(true);
   // наполняем содержимым
-  userElement.querySelector('.element__img').src = iniCardsURL[index];
-  userElement.querySelector('.element__txt').textContent = iniCardsTXT[index];//'Клон карты';
+  userElement.querySelector(".element__img").src = url;
+  userElement.querySelector(".element__txt").textContent = txt;//'Клон карты';
   // выберем кнопку лайка
-  userElement.querySelector('.element__like-btn').addEventListener('click', function (evt) {
-    evt.target.classList.toggle('element__like-btn_active');
+  userElement.querySelector(".element__like-btn").addEventListener("click", function (evt) {
+    evt.target.classList.toggle("element__like-btn_active");
   });
   // выберем кнопку удаления
-  userElement.querySelector('.element__del-btn').addEventListener('click', function (evt) {
+  userElement.querySelector(".element__del-btn").addEventListener("click", function (evt) {
     //console.log("Кн. УДАЛИТЬ");
-    const listItem = evt.target.closest('.element');
+    const listItem = evt.target.closest(".element");
     listItem.remove();
   });
   // отображаем на странице
   elementsOnline.append(userElement);//добавить в конец  
+}
 
+//Начальная загрузка страницы
+function iniElements () {
+  for (let index = 0; index < 6; index++) {
+    addElement (iniCardsURL[index],iniCardsTXT[index]);
+  }
+}
+iniElements();
+
+//Отладочная функция добавления карточки
+let index=0;
+function debugADD(){
+  addElement (iniCardsURL[index],iniCardsTXT[index]);
   if (index===5) {
     index=0;
   } else {
     index += 1;
   } 
-  //console.log(index);
+}
+
+// добавим обработчик кнопки +
+addButton.addEventListener("click", function() {
+  debugADD();
   console.log("кликнули по кнопке +");
 });
+
+
