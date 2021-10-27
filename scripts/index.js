96cf7043d8540a6b7ad49f75f801d1809b7e8172
Sher-Kon <sher-kon@ya.ru>
2021-10-27 16:04:51 +0300

    сделал второй попап

--- scripts/index.js
+++ scripts/index.js
@@ -3,12 +3,19 @@ const addButton = content.querySelector(".profile__add-btn");
 const editButton = content.querySelector(".profile__info-edit-btn");//кн.открытия формы
 const nameInfo = content.querySelector(".profile__info-name");
 const jobInfo = content.querySelector(".profile__info-job");
+// popup
 const popupElement = document.querySelector(".popup");
 const nameInput = document.querySelector(".popup__text_input_name");
 const jobInput = document.querySelector(".popup__text_input_job");
 const closeButton = document.querySelector(".popup__btn-close");//кн.закрытия формы
+// bildCard
+const bildCardElement = document.querySelector(".bildCard");
+const placeInput = document.querySelector(".bildCard__text_input_place");
+const urlInput = document.querySelector(".bildCard__text_input_url");
+const closeBttn = document.querySelector(".bildCard__btn-close");//кн.закрытия формы bildCard
 
-let formElement = document.querySelector(".form");// Находим форму в DOM
+const formElement = popupElement.querySelector(".form");// Находим форму в DOM
+const formbildCard = bildCardElement.querySelector(".form");// Находим форму в DOM in bildCardElement
 
 const initialCards = [
     {
@@ -53,18 +60,50 @@ const initialCards = [
     'Байкал',
   ];
 
-// Обработчик открытия формы
+
+// Обработчик открытия формы bildCard
+function openbildCard() {
+  //nameInput.value = nameInfo.textContent;
+  //jobInput.value = jobInfo.textContent;
+  bildCardElement.classList.add("bildCard_opened"); //открыть bildCard
+}
+// Обработчик закрытия формы bildCard
+function ClosebildCard() {
+  bildCardElement.classList.remove("bildCard_opened"); //закрыть bildCard
+}
+closeBttn.addEventListener("click", ClosebildCard);//закрыть bildCard
+// Обработчик «отправки» формы bildCard, хотя пока
+// она никуда отправляться не будет
+function bildCardSubmitHandler(evt) {
+  evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
+  // Так мы можем определить свою логику отправки.
+  // О том, как это делать, расскажем позже.
+  //console.log('Обработчик отправки формы');
+
+  // Вставьте новые значения с помощью textContent
+  //nameInfo.textContent = nameInput.value;
+  //jobInfo.textContent = jobInput.value;
+  debugADD();
+
+  // Закроем форму bildCard()
+  ClosebildCard();//закрыть окно bildCard()
+}
+// Прикрепляем обработчик к форме bildCard:
+// он будет следить за событием “submit” - «отправка»
+formbildCard.addEventListener("submit", bildCardSubmitHandler);
+
+
+
+// Обработчик открытия формы popup
 function clickEdit() {
     nameInput.value = nameInfo.textContent;
     jobInput.value = jobInfo.textContent;
     popupElement.classList.add("popup_opened"); //открыть окно
 }
-
-// Обработчик закрытия формы
+// Обработчик закрытия формы popup
 function clickClose() {
     popupElement.classList.remove("popup_opened"); //закрыть окно
 }
-
 // Обработчик «отправки» формы, хотя пока
 // она никуда отправляться не будет
 function formSubmitHandler(evt) {
@@ -76,16 +115,18 @@ function formSubmitHandler(evt) {
     // Вставьте новые значения с помощью textContent
     nameInfo.textContent = nameInput.value;
     jobInfo.textContent = jobInput.value;
-
+    
     // Закроем форму
     clickClose();//закрыть окно
 }
 // Прикрепляем обработчик к форме:
 // он будет следить за событием “submit” - «отправка»
 formElement.addEventListener("submit", formSubmitHandler);
-
+// Кнопки попапа
 editButton.addEventListener("click", clickEdit);//открыть попап
 closeButton.addEventListener("click", clickClose);//закрыть попап
+
+
 
 // Клонируем карточку
 function addElement (url, txt) {
@@ -108,7 +149,8 @@ function addElement (url, txt) {
     listItem.remove();
   });
   // отображаем на странице
-  elementsOnline.append(userElement);//добавить в конец  
+  //elementsOnline.append(userElement);//добавить в конец  
+  elementsOnline.prepend(userElement);//добавить в начало  
 }
 
 //Начальная загрузка страницы
@@ -132,7 +174,8 @@ function debugADD(){
 
 // добавим обработчик кнопки +
 addButton.addEventListener("click", function() {
-  debugADD();
+  //debugADD();
+  openbildCard();
   console.log("кликнули по кнопке +");
 });
 
