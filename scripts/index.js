6973c28b67c3e224b231243cdb9d17113cc50cbc
Sher-Kon <sher-kon@ya.ru>
2021-10-28 19:56:43 +0300

    причесал index.js

--- scripts/index.js
+++ scripts/index.js
@@ -22,48 +22,22 @@ const srcImg = lookImgElement.querySelector(".lookImg__img");
 const closelookImg = lookImgElement.querySelector(".lookImg__btn-close");//кн.закрытия формы lookImg
 const formlookImg = lookImgElement.querySelector(".form");// Находим форму в DOM in lookImgElement
 
-const initialCards = [
-    {
-      name: 'Архыз',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-    },
-    {
-      name: 'Челябинская область',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-    },
-    {
-      name: 'Иваново',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-    },
-    {
-      name: 'Камчатка',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-    },
-    {
-      name: 'Холмогорский район',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-    },
-    {
-      name: 'Байкал',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-    }
-  ];
-  const iniCardsURL = [
-    'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg',
-    'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg',
-    'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg',
-    'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg',
-    'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg',
-    'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-  ];
-  const iniCardsTXT = [
-    'Архыз',
-    'Челябинская область',
-    'Иваново',
-    'Камчатка',
-    'Холмогорский район',
-    'Байкал',
-  ];
+const iniCardsURL = [
+  'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg',
+  'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg',
+  'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg',
+  'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg',
+  'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg',
+  'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+];
+const iniCardsTXT = [
+  'Архыз',
+  'Челябинская область',
+  'Иваново',
+  'Камчатка',
+  'Холмогорский район',
+  'Байкал',
+];
 
 // Обработчик открытия формы lookImg
 function openlookImg() {
@@ -73,48 +47,42 @@ function openlookImg() {
 function CloselookImg() {
   lookImgElement.classList.remove("lookImg_opened"); //закрыть lookImg
 }
+// Кнопка - Х -закрыть "lookImg"
 closelookImg.addEventListener("click", CloselookImg);//закрыть lookImg
-
-  
 
 // Обработчик открытия формы bildCard
 function openbildCard() {
-  //nameInput.value = nameInfo.textContent;
-  //jobInput.value = jobInfo.textContent;
   bildCardElement.classList.add("bildCard_opened"); //открыть bildCard
 }
 // Обработчик закрытия формы bildCard
 function ClosebildCard() {
   bildCardElement.classList.remove("bildCard_opened"); //закрыть bildCard
 }
-closeBttn.addEventListener("click", ClosebildCard);//закрыть bildCard
-
-// Обработчик «отправки» формы bildCard, хотя пока
-// она никуда отправляться не будет
+// Обработчик «отправки» формы bildCard
 function bildCardSubmitHandler(evt) {
   evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
-  // Так мы можем определить свою логику отправки.
-  // О том, как это делать, расскажем позже.
-  //console.log('Обработчик отправки формы');
-
   // Вставьте новые значения в новую карточку
   const place = placeInput.value;
   const url = urlInput.value;
   //console.log(url);//debug
   if (url!=='') {
-    //debugADD();
-    addElement(url, place);//клонируем карточку
+    addElement(url, place, "up");//клонируем карточку в начало
   }
   // Закроем форму bildCard()
   ClosebildCard();//закрыть окно bildCard()
 }
-// Прикрепляем обработчик к форме bildCard:
-// он будет следить за событием “submit” - «отправка»
+// форма bildCard:
+// Кнопка - «создать»
 formbildCard.addEventListener("submit", bildCardSubmitHandler);
+// Кнопка - Х -закрыть
+closeBttn.addEventListener("click", ClosebildCard);//закрыть bildCard
+// Кнопка « + » (открыть окно "Новое место")
+addButton.addEventListener("click", function() {
+  //console.log("кликнули по кнопке +");//отладка
+  openbildCard();
+});
 
-
-
-// Обработчик открытия формы popup
+// Обработчик открытия формы popup «Редактировать профиль»
 function clickEdit() {
     nameInput.value = nameInfo.textContent;
     jobInput.value = jobInfo.textContent;
@@ -130,27 +98,23 @@ function formSubmitHandler(evt) {
     evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
     // Так мы можем определить свою логику отправки.
     // О том, как это делать, расскажем позже.
-    //console.log('Обработчик отправки формы');
 
     // Вставьте новые значения с помощью textContent
     nameInfo.textContent = nameInput.value;
-    jobInfo.textContent = jobInput.value;
-    
+    jobInfo.textContent = jobInput.value;    
     // Закроем форму
     clickClose();//закрыть окно
 }
-// Прикрепляем обработчик к форме:
+// Прикрепляем обработчик к форме «Редактировать профиль»:
 // он будет следить за событием “submit” - «отправка»
 formElement.addEventListener("submit", formSubmitHandler);
 // Кнопки попапа
 editButton.addEventListener("click", clickEdit);//открыть попап
 closeButton.addEventListener("click", clickClose);//закрыть попап
 
-
-
 // Клонируем карточку
-function addElement (url, txt) {
-  // получим элемент 
+function addElement (url, txt, direction) {
+  // получим элементы DOM 
   const elementTemplate = document.querySelector("#element").content;//клон
   const elementsOnline = document.querySelector(".elements");//куда вставить
   // клонируем содержимое тега template
@@ -158,12 +122,12 @@ function addElement (url, txt) {
   // наполняем содержимым
   userElement.querySelector(".element__img").src = url;
   userElement.querySelector(".element__txt").textContent = txt;//'Клон карты';
-  // выберем кнопку увеличить картинку
+  // выберем кнопку увеличить картинку "lookImg"
   userElement.querySelector(".element__img-btn").addEventListener("click", function (evt) {
     txtImg.textContent = evt.target.closest(".element").textContent;
     srcImg.src = evt.target.closest(".element__img").src;
-      //console.log("Кн. Увеличить картинку");
-    openlookImg();//открыть окно просмотра картинки
+      //console.log("Кликнули по картинке");//отладка
+    openlookImg();//открыть окно просмотра картинки "lookImg"
   });
   // выберем кнопку лайка
   userElement.querySelector(".element__like-btn").addEventListener("click", function (evt) {
@@ -176,34 +140,18 @@ function addElement (url, txt) {
     listItem.remove();
   });
   // отображаем на странице
-  //elementsOnline.append(userElement);//добавить в конец  
-  elementsOnline.prepend(userElement);//добавить в начало  
+  if (direction==="dn") {
+    elementsOnline.append(userElement);//добавить в конец 
+  }
+  if (direction==="up") {
+    elementsOnline.prepend(userElement);//добавить в начало  
+  }
 }
 
-//Начальная загрузка страницы
+//Начальная загрузка страницы - 6 карточек
 function iniElements () {
   for (let index = 0; index < 6; index++) {
-    addElement (iniCardsURL[index],iniCardsTXT[index]);
+    addElement (iniCardsURL[index],iniCardsTXT[index],"dn");
   }
 }
 iniElements();
-
-//Отладочная функция добавления карточки
-let index=0;
-function debugADD(){
-  addElement (iniCardsURL[index],iniCardsTXT[index]);
-  if (index===5) {
-    index=0;
-  } else {
-    index += 1;
-  } 
-}
-
-// добавим обработчик кнопки +
-addButton.addEventListener("click", function() {
-  //console.log("кликнули по кнопке +");//отладка
-  //debugADD();
-  openbildCard();
-});
-
-
