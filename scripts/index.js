4dbac5496e25fc53d24fa17ce781d3f6177db12c
Sher-Kon <sher-kon@ya.ru>
2021-11-23 12:27:42 +0300

    сделад обработчики кликов

--- scripts/index.js
+++ scripts/index.js
@@ -62,11 +62,27 @@ class Card {
     this._element.querySelector('.element__like-btn').addEventListener('click', () => {
       this._likeClick();
     });
+    this._element.querySelector('.element__del-btn').addEventListener('click', () => {
+      this._closeClick();
+    });
+    this._element.querySelector('.element__img-btn').addEventListener('click', () => {
+      this._lookClick();
+    });
   }
 
   // обработчик лайка
   _likeClick() {
     this._element.querySelector('.element__like-btn').classList.toggle('element__like-btn_active');
+  }  
+  // обработчик close
+  _closeClick() {
+    this._element.remove();
+  }  
+  // обработчик look
+  _lookClick() {
+    txtImg.textContent = this._element.querySelector('.element__txt').textContent;
+    srcImg.src = this._element.querySelector('.element__img').src;
+    openLookImg();//открыть окно просмотра картинки "lookImg"
   }  
 }
 
