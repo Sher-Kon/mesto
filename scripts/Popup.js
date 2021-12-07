3abcc6be794dea3de6bfe65c2334cf126d571ef5
Sher-Kon <sher-kon@ya.ru>
2021-12-07 15:59:38 +0300

    добавил классы

---
+++ scripts/Popup.js
@@ -0,0 +1,23 @@
+export default class Popup {
+    constructor(popupSelector) {
+        this._popup = document.querySelector(popupSelector);
+    }
+
+    open() {
+
+    }
+
+    close() {
+
+    }
+    
+    // Закрытие попапа клавишей Esc
+    _handleEscClose() { 
+
+    }
+
+    // Добавляет слушатель клика иконке закрытия попапа
+    setEventListeners() {
+
+    }
+}    
