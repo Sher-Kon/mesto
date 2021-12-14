464632e66214c47ed4079d6b510bca3169e785c3
Sher-Kon <sher-kon@ya.ru>
2021-12-14 08:21:47 +0300

    удалил коментарии

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
 		filename: 'main.js',
 		publicPath: ''
 	},
-    mode: 'development',
+	mode: 'development',
 	devServer: {
 		static: {
 			directory: path.join(__dirname, 'dist'),
@@ -26,47 +26,37 @@ module.exports = {
 				use: 'babel-loader',
 				exclude: /node_modules/
 			},
-			  // �������� ������� ��� ��������� ������ ��������
 			{
-				// ���������� ���������, ������� ���� ��� ����� � ������ ������������
 				test: /\.(png|svg|jpg|gif)$/i,
 				type: 'asset/resource',
 				generator: {
 					filename: 'images/[name].[hash][ext]'
 				}
 			},
-			  // �������� ������� ��� ��������� ������ �������
 			{
-				// ���������� ���������, ������� ���� ��� ����� � ������ ������������
 				test: /\.(woff|woff2|eot|ttf|otf)$/i,
 				type: 'asset/resource',
 				generator: {
 					filename: 'fonts/[name].[hash][ext]'
 				}
 			},
-		  // ������� ��� css:
 			{
-				// ��������� ��� ������� ������ � CSS-������
 				test: /\.css$/i,
-				// ��� ��������� ���� ������ ����� ������������
-				// MiniCssExtractPlugin.loader � css-loader
-				use: [MiniCssExtractPlugin.loader, 
+				use: [MiniCssExtractPlugin.loader,
 				{
-				  loader: 'css-loader',
-					// �������� ������ options
-				  options: { importLoaders: 1 }
+					loader: 'css-loader',
+					options: { importLoaders: 1 }
 				},
-				// �������� postcss-loader
-				'postcss-loader'
+					'postcss-loader'
 				]
 			},
 		],
 	},
 	plugins: [
 		new HtmlWebpackPlugin({
-		  template: './src/index.html'
+			template: './src/index.html'
 		}),
 		new CleanWebpackPlugin(),
-		new MiniCssExtractPlugin(), // подключение плагина для объединения файлов
+		new MiniCssExtractPlugin(),
 	]
 };
