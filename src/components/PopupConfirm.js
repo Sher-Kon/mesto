20b5114380660d7637a953ac460ff06ddc682ef3
Sher-Kon <sher-kon@ya.ru>
2021-12-31 14:54:52 +0300

    сделал версию 3.61

--- src/components/PopupConfirm.js
+++ src/components/PopupConfirm.js
@@ -4,7 +4,7 @@ export class PopupConfirm extends Popup {
     constructor(popupSelector, handleConfirm) {
         super(popupSelector);
         this._handleConfirm = handleConfirm;
-        this._btnElement = this._popupElement.querySelector(".confirm__btn-save");// Находим кнопку в DOM
+        this._btnElement = this._popupElement.querySelector(".confirm__btn");// Находим кнопку в DOM
     }
 
     // Открытие попапа
@@ -19,6 +19,6 @@ export class PopupConfirm extends Popup {
     setEventListeners() {
         super.setEventListeners();
         // Добавляет обработчик клика кнопки
-        this._btnElement.addEventListener("click", () =>  { this._handleConfirm(this._card) });//this._card.bind(this)
+        this._btnElement.addEventListener("click", () =>  { this._handleConfirm(this._card) });
     }
 }
