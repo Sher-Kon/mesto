9a5b0578289b27a4926a2a6830285ece05b19d84
Sher-Kon <sher-kon@ya.ru>
2022-01-01 18:52:11 +0300

    сделал версию 3.87

--- src/pages/index.js
+++ src/pages/index.js
@@ -330,7 +330,7 @@ api.getIniData().then(arg => {
   //--------------------------------------------------------
   const countCard = dataCards.length;//загрузим все
   console.log("Загрузим карточек: " + countCard);//
-  //const countCard = 6;//начальная загрузка
+  countCard = 16;//начальная загрузка
   for (let i = 0; i < countCard; i += 1) {
     rdCards[i].name = dataCards[i].name;//
     rdCards[i].link = dataCards[i].link;
