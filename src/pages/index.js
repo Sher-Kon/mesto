63e7d832baafbede1f4592416cb75914603869b7
Sher-Kon <sher-kon@ya.ru>
2022-01-01 19:19:49 +0300

    сделал версию 3.91

--- src/pages/index.js
+++ src/pages/index.js
@@ -329,8 +329,7 @@ api.getIniData().then(arg => {
   //  Начальная загрузка страницы - 6 карточек
   //--------------------------------------------------------
   console.log("Всего карточек: " + dataCards.length);//
-  const counter = 16;//начальная загрузка
-  for (let i = 0; i < counter; i += 1) {
+  for (let i = 0; i < 16; i += 1) {
     rdCards[i].name = dataCards[i].name;//
     rdCards[i].link = dataCards[i].link;
     rdCards[i].myID = dataProfile._id;
